1a521687c7676a4b60ef8105fb8aad218f4d8e38
Veronica Ayars <veronica.ayars@gmail.com>
2022-02-11 16:56:31 -0600

    Added retry button to game over screen

--- game3/simple-picross/src/GamePanel.java
+++ game3/simple-picross/src/GamePanel.java
@@ -52,37 +52,39 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void draw(Graphics g) {
 	if (running) {
-	    g.setColor(Color.darkGray);
-	    for (int i = 2; i < (SCREEN_HEIGHT / UNIT_SIZE) - 1; i++) {
-		// g.drawLine(x1, y1, x2, y2);
-		g.drawLine(i * UNIT_SIZE, UNIT_SIZE, i * UNIT_SIZE, SCREEN_HEIGHT - (2 * UNIT_SIZE));
-		g.drawLine(UNIT_SIZE, i * UNIT_SIZE, SCREEN_WIDTH - (2 * UNIT_SIZE), i * UNIT_SIZE);
-	    }
-	    drawHealth(g);
-	    g.setColor(Color.white);
-	    g.drawRect(BOARD_X, BOARD_Y, BOARD_WIDTH, BOARD_HEIGHT);
-	    if (currentLevel == 1) {
-		drawLevelOne(g);
-	    }
-	    // Draw the yes, nos, and marks.
-	    g.setColor(Color.yellow);
-	    for (int i = 0; i < markX.size(); i++) {
-		g.drawLine(markX.get(i), markY.get(i), markX.get(i) + UNIT_SIZE, markY.get(i) + UNIT_SIZE);
-		g.drawLine(markX.get(i), markY.get(i) + UNIT_SIZE, markX.get(i) + UNIT_SIZE, markY.get(i));
-	    }
-	    g.setColor(Color.white);
-	    for (int j = 0; j < yesX.size(); j++) {
-		g.fillRect(yesX.get(j), yesY.get(j), UNIT_SIZE, UNIT_SIZE);
-	    }
-	    g.setColor(Color.red);
-	    for (int k = 0; k < noX.size(); k++) {
-		g.drawLine(noX.get(k), noY.get(k), noX.get(k) + UNIT_SIZE, noY.get(k) + UNIT_SIZE);
-		g.drawLine(noX.get(k), noY.get(k) + UNIT_SIZE, noX.get(k) + UNIT_SIZE, noY.get(k));
-	    }
+		g.setColor(Color.darkGray);
+		for (int i = 2; i < (SCREEN_HEIGHT / UNIT_SIZE) - 1; i++) {
+		    // g.drawLine(x1, y1, x2, y2);
+		    g.drawLine(i * UNIT_SIZE, UNIT_SIZE, i * UNIT_SIZE, SCREEN_HEIGHT - (2 * UNIT_SIZE));
+		    g.drawLine(UNIT_SIZE, i * UNIT_SIZE, SCREEN_WIDTH - (2 * UNIT_SIZE), i * UNIT_SIZE);
+		}
+		drawHealth(g);
+		g.setColor(Color.white);
+		g.drawRect(BOARD_X, BOARD_Y, BOARD_WIDTH, BOARD_HEIGHT);
+		if (currentLevel == 1) {
+		    drawLevelOne(g);
+		}
+		// Draw the yes, nos, and marks.
+		g.setColor(Color.yellow);
+		for (int i = 0; i < markX.size(); i++) {
+		    g.drawLine(markX.get(i), markY.get(i), markX.get(i) + UNIT_SIZE, markY.get(i) + UNIT_SIZE);
+		    g.drawLine(markX.get(i), markY.get(i) + UNIT_SIZE, markX.get(i) + UNIT_SIZE, markY.get(i));
+		}
+		g.setColor(Color.white);
+		for (int j = 0; j < yesX.size(); j++) {
+		    g.fillRect(yesX.get(j), yesY.get(j), UNIT_SIZE, UNIT_SIZE);
+		}
+		g.setColor(Color.red);
+		for (int k = 0; k < noX.size(); k++) {
+		    g.drawLine(noX.get(k), noY.get(k), noX.get(k) + UNIT_SIZE, noY.get(k) + UNIT_SIZE);
+		    g.drawLine(noX.get(k), noY.get(k) + UNIT_SIZE, noX.get(k) + UNIT_SIZE, noY.get(k));
+		}
 	} else {
 	    if (health == 0) {
+		currentLevel = -1;
 		gameOver(g);
 	    } else {
+		currentLevel = -2;
 		complete(g);
 	    }
 	}
@@ -180,12 +182,18 @@ public class GamePanel extends JPanel implements ActionListener {
 	}
     }
 
+
     public void gameOver(Graphics g) {
 	g.setColor(Color.red);
 	g.setFont(new Font("Ink Free", Font.BOLD, 75));
-	FontMetrics metrics = getFontMetrics(g.getFont());
-	g.drawString("Game Over", (SCREEN_WIDTH - metrics.stringWidth("Game Over")) / 2, SCREEN_HEIGHT / 2);
+	FontMetrics metrics1 = getFontMetrics(g.getFont());
+	g.drawString("Game Over", (SCREEN_WIDTH - metrics1.stringWidth("Game Over")) / 2, SCREEN_HEIGHT / 2);
 
+	g.fillRect((SCREEN_WIDTH / 2) - ((3 * UNIT_SIZE) / 2), 6 * UNIT_SIZE, 3 * UNIT_SIZE, UNIT_SIZE);
+	g.setColor(Color.black);
+	g.setFont(new Font("Ink Free", Font.BOLD, 30));
+	FontMetrics metrics2 = getFontMetrics(g.getFont());
+	g.drawString("Retry?", (SCREEN_WIDTH - metrics2.stringWidth("Retry")) / 2, (int) ((13 * UNIT_SIZE) / 2));
     }
 
     public void complete(Graphics g) {
@@ -193,7 +201,6 @@ public class GamePanel extends JPanel implements ActionListener {
 	g.setFont(new Font("Ink Free", Font.BOLD, 75));
 	FontMetrics metrics = getFontMetrics(g.getFont());
 	g.drawString("Congrats!", (SCREEN_WIDTH - metrics.stringWidth("Congrats!")) / 2, SCREEN_HEIGHT / 2);
-
     }
 
     @Override
@@ -213,36 +220,53 @@ public class GamePanel extends JPanel implements ActionListener {
 	    int gridX = ((int) (mouseX / UNIT_SIZE)) * UNIT_SIZE;
 	    int gridY = ((int) (mouseY / UNIT_SIZE)) * UNIT_SIZE;
 	    int button = e.getButton();
-	    System.out.println("Mouse: " + mouseX + " " + mouseY + ". Grid: " + gridX + " " + gridY);
-	    boolean empty = true;
-	    // Check to make sure there's no yes or nos in that spot
-	    for (int i = 0; i < yesX.size(); i++) {
-		if (gridX == yesX.get(i) && gridY == yesY.get(i)) {
-		    empty = false;
-		    break;
-		}
-	    }
-	    for (int j = 0; j < noX.size(); j++) {
-		if (gridX == noX.get(j) && gridY == noY.get(j)) {
-		    empty = false;
-		    break;
-		}
-	    }
-	    if (gridX < BOARD_X || gridX > (BOARD_X + BOARD_WIDTH - 1) || gridY < BOARD_Y
-		    || gridY > (BOARD_Y + BOARD_HEIGHT - 1)) {
-		empty = false;
-	    }
-	    // If the spot is open...
-	    if (empty) {
-		// If left click, check if correct
+	    // System.out.println("Mouse: " + mouseX + " " + mouseY + ". Grid: " + gridX + "
+	    // " + gridY);
+	    if (currentLevel == 1) {
+        	    boolean empty = true;
+        	    // Check to make sure there's no yes or nos in that spot
+        	    for (int i = 0; i < yesX.size(); i++) {
+        		if (gridX == yesX.get(i) && gridY == yesY.get(i)) {
+        		    empty = false;
+        		    break;
+        		}
+        	    }
+        	    for (int j = 0; j < noX.size(); j++) {
+        		if (gridX == noX.get(j) && gridY == noY.get(j)) {
+        		    empty = false;
+        		    break;
+        		}
+        	    }
+        	    if (gridX < BOARD_X || gridX > (BOARD_X + BOARD_WIDTH - 1) || gridY < BOARD_Y
+        		    || gridY > (BOARD_Y + BOARD_HEIGHT - 1)) {
+        		empty = false;
+        	    }
+        	    // If the spot is open...
+        	    if (empty) {
+        		// If left click, check if correct
+        		if (button == MouseEvent.BUTTON1) {
+        		    // System.out.println("Left click.");
+        		    checkCorrect(gridX, gridY);
+        		}
+        		// If right click, create a marker
+        		else if (button == MouseEvent.BUTTON3) {
+        		    // System.out.println("Right click");
+        		    mark(gridX, gridY);
+        		}
+        	    }
+	    } else if (currentLevel == -1) {
 		if (button == MouseEvent.BUTTON1) {
-		    System.out.println("Left click.");
-		    checkCorrect(gridX, gridY);
-		}
-		// If right click, create a marker
-		else if (button == MouseEvent.BUTTON3) {
-		    System.out.println("Right click");
-		    mark(gridX, gridY);
+		    if (gridX >= 3 * UNIT_SIZE && gridX <= 5 * UNIT_SIZE && gridY == 6 * UNIT_SIZE) {
+			running = true;
+			currentLevel = 1;
+			yesX.clear();
+			yesY.clear();
+			noX.clear();
+			noY.clear();
+			markX.clear();
+			markY.clear();
+			health = 3;
+		    }
 		}
 	    }
 	    repaint();
